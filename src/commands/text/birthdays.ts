d934dc496f5df6d6ec2cb67b71da5f84da93d707
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2023-06-25 20:53:02 +0200

    Make birthdays smaller

--- src/commands/text/birthdays.ts
+++ src/commands/text/birthdays.ts
@@ -1,12 +1,12 @@
-import type { TextCommand } from "discord.js";
-import { EmbedBuilder } from "discord.js";
-import { BirthdayModel } from "../../models/Birthday.js";
+import type {TextCommand} from "discord.js";
+import {EmbedBuilder} from "discord.js";
+import {BirthdayModel} from "../../models/Birthday.js";
 
 export const command: TextCommand = {
     name: "birthdays",
 
     async execute(message) {
-        const birthdays = await BirthdayModel.find().sort({ date: 1 });
+        const birthdays = await BirthdayModel.find().sort({date: 1});
 
         if (birthdays.length === 0) {
             const embed = new EmbedBuilder()
@@ -14,14 +14,14 @@ export const command: TextCommand = {
                 .setDescription("No birthdays have been set")
                 .setColor(0xED4245);
 
-            await message.reply({ embeds: [embed] });
+            await message.reply({embeds: [embed]});
         }
 
         let description = "";
 
         for (const birthday of birthdays) {
             const timestamp = Math.round(birthday.date.getTime() / 1000);
-            description += `<@${birthday.user}>\n<t:${timestamp}:R> (<t:${timestamp}:d>)\n\n`;
+            description += `<@${birthday.user}> - <t:${timestamp}:R> (<t:${timestamp}:d>)\n`;
         }
 
         const embed = new EmbedBuilder()
@@ -29,6 +29,6 @@ export const command: TextCommand = {
             .setDescription(description)
             .setColor(0xEB459E);
 
-        await message.reply({ embeds: [embed] });
+        await message.reply({embeds: [embed]});
     },
 };
