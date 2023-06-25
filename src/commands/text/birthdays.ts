5bbf354294e2ef7e6510804ed75b9bdc9cc09b9c
Sam Teeuwisse <sam.teeuwisse2@gmail.com>
2023-06-25 20:56:38 +0200

    Allow checking one person's birthday

--- src/commands/text/birthdays.ts
+++ src/commands/text/birthdays.ts
@@ -6,6 +6,30 @@ export const command: TextCommand = {
     name: "birthdays",
 
     async execute(message) {
+        if (message.mentions.users.size > 0) {
+            let mention = message.mentions.users.first()!;
+            const birthday = await BirthdayModel.findOne({user: mention.id});
+
+            if (!birthday) {
+                const embed = new EmbedBuilder()
+                    .setTitle("❌ Error")
+                    .setDescription("This user does not have a birthday set")
+                    .setColor(0xED4245);
+
+                await message.reply({embeds: [embed]});
+                return;
+            }
+
+            const timestamp = Math.round(birthday.date.getTime() / 1000);
+            const embed = new EmbedBuilder()
+                .setTitle(`🎂 ${mention.username}'s birthday`)
+                .setDescription(`<t:${timestamp}:R> (<t:${timestamp}:d>)`)
+                .setColor(0xEB459E);
+
+            await message.reply({embeds: [embed]});
+            return;
+        }
+
         const birthdays = await BirthdayModel.find().sort({date: 1});
 
         if (birthdays.length === 0) {
